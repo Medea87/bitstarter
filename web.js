81bf7391e2c183484f752b7293ae1c2590f2897f
John Smith <example@stanford.edu>
2013-07-14 10:56:53 +0000

    Reading index.html from web.js, v2

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buff = require('fs');
+var fs = require('fs');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-	buff = fs.readFileSync('index.html');
-	response.send(buff.toString());
+	fs.readFileSync('index.html');
+	response.send(fs.toString());
 });
 
 var port = process.env.PORT || 5000;
